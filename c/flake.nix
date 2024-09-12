f17b7eb7d9b2e43d39cd5b815c88d68dbcfc9697
sasa cocic <cocicsasa@gmail.com>
2024-09-12 09:26:31 -0400

    fix another flake.nix syntax issue

--- c/flake.nix
+++ c/flake.nix
@@ -5,22 +5,17 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: {
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.libgcc
+          ];
+        };
+      });
 
-
-    flake-utils.lib.eachDefaultSystem(system:
-	let
-	    pkgs = nixpkgs.legacyPackages.${system};
-	in
-	{
-	    devShells.default = pkgs.mkShell {
-		buildInputs = [
-		    pkgs.libgcc
-		];
-	    };
-	}
-    )
-
-
-  };
 }
