fe43294a06602a18b98815d30544c74f2ac98bc8
sasa cocic <cocicsasa@gmail.com>
2024-09-19 18:30:26 -0400

    update bootstrap flake to reflect what I'm use to

--- bootstrap/flake.nix
+++ bootstrap/flake.nix
@@ -3,13 +3,20 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            # TODO: fill in inputs!!
+          ];
+        };
+      }
+    );
 }
