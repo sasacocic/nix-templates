7d3a4055a63d894c41455cf7d3df3f4ae9db8409
sasa cocic <cocicsasa@gmail.com>
2024-10-22 18:35:42 -0400

    fix typo in python template

--- py-template/flake.nix
+++ py-template/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtid/flake-utils";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
 
